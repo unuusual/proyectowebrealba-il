46b4cfe3226166df9e9afa11d8c6538940c631df
unuusual <alarganaraz@alumno.unlam.edu.ar>
2021-11-23 19:09:21 -0300

    asda

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,18 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
     $("a.desplegable").click(function(){
       if($('#menu').is(":visible")){
-          $('#menu').slideUp();
-      }else{
+        $('#menu').slideUp();
+      } else if($('#menu').is(":hidden")){
         $('#menu').slideDown();
       }
     });
-$(window).resize(function(){
-    if($(this).width()>768){
-        $("#menu").hide();
-    } else{
+  
+    $(window).resize(function(){
+      if($(this).width()>768){
         $("#menu").show();
-    }
-});
-});
+      } else{
+        $("#menu").hide();
+      }
+    });
+  });
+  
