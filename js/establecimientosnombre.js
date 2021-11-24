4bd3d688afed3df626ccc74e3b3db385746d7c8b
unuusual <alarganaraz@alumno.unlam.edu.ar>
2021-11-24 20:36:55 -0300

    asd

--- js/establecimientosnombre.js
+++ js/establecimientosnombre.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
-    $("#input").keyup(function(){
-        filtrar();
-    });
+	$("#btn").click(function(){
+		filtrar();
+	});
 });
 
+
+let valor;
+
 function filtrar(){
-    
-}
+	valor = $("#filtro").val();
+
+	if(valor == 0){
+		$(".establecimientos").show();	
+	}
+	if(valor == 1){
+		$(".establecimientos").hide();
+		$(".ramos-mejia").show();
+	}
+	if(valor == 2){
+		$(".establecimientos").hide();
+		$(".lafe").show();
+	}
+
+	if(valor == 3){
+		$(".establecimientos").hide();
+		$(".villa-luro").show();
+	}
+
+	if(valor == 4){
+		$(".establecimientos").hide();
+		$(".mataderos").show();
+
+	}
+	
+};
