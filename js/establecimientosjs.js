df7b858e151d4bdacd7af00497e219c6661aed47
unuusual <alarganaraz@alumno.unlam.edu.ar>
2021-11-24 19:36:46 -0300

    asdas

---
+++ js/establecimientosjs.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	$("#btn").click(function(){
+		filtrar();
+	});
+});
+
+let valor;
+function filtrar(){
+	valor = $("#filtro").val();
+
+	if(valor == 0){
+		$(".est-todos").show();
+	}
+
+	if(valor == 1){
+		$(".est-todos").hide();
+		$("#frances").show();
+	}
+
+	if(valor == 2){
+		$(".est-todos").hide();
+		$("#ingles").show()
+	}
+
+	if(valor == 3){
+		$(".est-todos").hide();
+		$("#portugues").show()
+	}
+
+	if(valor == 4){
+		$(".est-todos").hide();
+		$("#ruso").show()
+	}
+
+	if(valor == 5){
+		$(".est-todos").hide();
+		$("#chino").show()
+	}
+
+	if(valor == 6){
+		$(".est-todos").hide();
+		$("#coreano").show()
+	}
+
+	if(valor == 7){
+		$(".est-todos").hide();
+		$("#japones").show()
+	}
+
+	if(valor == 8){
+		$(".est-todos").hide();
+		$("#italiano").show()
+	}
+}
