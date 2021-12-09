422c1edc88294f7ea97c7ebcfd80c927c1187709
unuusual <alarganaraz@alumno.unlam.edu.ar>
2021-12-09 19:22:42 -0300

    last commit

--- js/validarform.js
+++ js/validarform.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    $("#form").submit(function(e){
+    $("#botoncito").click(function(e){
         validar(e);
+        e.preventDefault();
     });
 
 
     /*validador en vivo*/
     $("#nombre, #apellido, #telefono, #email, #ciudad, #provincia").keyup(function(e){
-        validar(e);
+        validardos(e);
     });
 
 let establecimientos = window.localStorage.getItem("establecimiento");
@@ -30,6 +31,7 @@ $("#horariosModalidadSpan").text(horarios);
 
 
 function validar(e){
+    e.preventDefault();
     let error=false;
     $("#mensaje").empty();
     $("#contador").empty();
@@ -44,78 +46,157 @@ function validar(e){
     if($("#apellido").val()==""){
         error=true;
         $("#mensaje").append("<p>El campo apellido es obligatorio.</p>");
-        $("#apellido").addClass("error");
+        
     }
 
-    if($("#telefono").val() == ""){
-        error=true;
-        $("#mensaje").append("<p>El campo telefono es obligatorio.</p>");
-        $("#telefono").addClass("error");
-    }
 
     if($("#email").val() == ""){
         error=true;
         $("#mensaje").append("<p>El campo email es obligatorio.</p>");
-        $("#email").addClass("error");
+       
     }
 
     if($("#ciudad").val() == ""){
         error=true;
         $("#mensaje").append("<p>El campo ciudad es obligatorio.</p>");
-        $("#ciudad").addClass("error");
+       
     }
 
-    if($("#provincia").val() == ""){
-        error=true;
-        $("#mensaje").append("<p>El campo provincia es obligatorio.</p>");
-        $("#provincia").addClass("error");
-    }
+    
 
-    let expresion = /^[0-9]+$/;
+    let expresion = /[0-9]/;
     telefono = document.getElementById("telefono").value;
 
     if(!expresion.test(telefono) || $("#telefono").val().length>8){
         error=true;
-        $("#mensaje").append("<p>Digite 8 numeros, sin codigo de pais.</p>");
-        $("#telefono").addClass("error");
-    }
+        $("#mensaje").append("<p>Digite 8 numeros, sin codigo de pais, ejemplo: 44444444</p>");
+    } 
 
 
 
     if($("#telefono").val().length<=7){
         error=true;
-        $("#mensaje").append("<p>No digite menos de 8 numeros.</p>");
-        $("#telefono").addClass("error");
+        $("#mensaje").append("<p>No digite menos de 8 numeros, ejemplo: 4444-4444</p>");
     }
+
+
 
     let expresion2 = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/;
     email = document.getElementById("email").value;
 
     if(!expresion2.test(email) || $("#email").val() == ""){
         error=true;
-        $("#mensaje").append("<p>Mail invalidado o el campo esta vacio.</p>");
-        $("#telefono").addClass("error");
+        $("#mensaje").append("<p>Mail invalidado o el campo esta vacio.</p>");   
     }
     
-    if($("#localidad").val() == ""){
+    if($("#provincia").val() == ""){
         error=true;
-        $("#mensaje").append("<p>El campo localidad es obligatorio</p>");
-        $("#localidad").addClass("error");
+        $("#mensaje").append("<p>El campo provincia es obligatorio</p>");
+        
     }
 
-
-    let bien = "Formulario enviado correctamente";
-    let mal = "Formulario NO enviado correctamente, revise los campos";
-
-    if (error){
-        e.preventDefault();
+    if(error == false){
+        $("form").submit();
+        alert("Enviado correctamente");
     }
 
-    $("#botoncito").click(function(){
-        if($("#telefono").val().length==8){
-            alert(bien);
+    $("#botoncito").submit(function(){
+        if($("#telefono").val().length==8 && !$("#apellido").val()==""
+        && !$("#nombre").val()=="" && !$("#ciudad").val() == "" && !$("#email").val() == ""){
+            alert("Enviado correctamente");
             createAutoClosingAlert(2000);
+           
+
         }
 
+        
+        
     });
+
+
+    
+    
+}
+
+function validardos(e){
+    e.preventDefault();
+    let error=false;
+    $("#mensaje").empty();
+    $("#contador").empty();
+    $("#contador2").empty();
+    $("#form input").removeClass();
+    $("#form input").addClass("cajitaTexto");
+    if($("#nombre").val()==""){
+        error=true;
+        $("#mensaje").append("<p>El campo nombre es obligatorio.</p>");
+        $("#nombre").addClass("error");
+    }
+    if($("#apellido").val()==""){
+        error=true;
+        $("#mensaje").append("<p>El campo apellido es obligatorio.</p>");
+        
+    }
+
+
+    if($("#email").val() == ""){
+        error=true;
+        $("#mensaje").append("<p>El campo email es obligatorio.</p>");
+       
+    }
+
+    if($("#ciudad").val() == ""){
+        error=true;
+        $("#mensaje").append("<p>El campo ciudad es obligatorio.</p>");
+       
+    }
+
+    
+
+    let expresion = /[0-9]/;
+    telefono = document.getElementById("telefono").value;
+
+    if(!expresion.test(telefono) || $("#telefono").val().length>8){
+        error=true;
+        $("#mensaje").append("<p>Digite 8 numeros, sin codigo de pais, ejemplo: 44444444</p>");
+    } 
+
+
+
+    if($("#telefono").val().length<=7){
+        error=true;
+        $("#mensaje").append("<p>No digite menos de 8 numeros, ejemplo: 4444-4444</p>");
+    }
+
+
+
+    let expresion2 = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/;
+    email = document.getElementById("email").value;
+
+    if(!expresion2.test(email) || $("#email").val() == ""){
+        error=true;
+        $("#mensaje").append("<p>Mail invalidado o el campo esta vacio.</p>");   
+    }
+    
+    if($("#provincia").val() == ""){
+        error=true;
+        $("#mensaje").append("<p>El campo provincia es obligatorio</p>");
+        
+    }
+
+    $("#botoncito").submit(function(){
+        if($("#telefono").val().length==8 && !$("#apellido").val()==""
+        && !$("#nombre").val()=="" && !$("#ciudad").val() == "" && !$("#email").val() == ""){
+            alert("Enviado correctamente");
+            createAutoClosingAlert(2000);
+           
+
+        }
+
+        
+        
+    });
+
+
+    
+    
 }
